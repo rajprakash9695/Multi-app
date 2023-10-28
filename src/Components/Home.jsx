17add262083323a57e83e3dd4f8fa0ada2d1e694
Raj Prakash Maurya <raj.maurya@bksgold.com>
2023-10-28 13:49:00 +0530

    home completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,18 @@
-import { Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 
 function Home() {
   return (
-    <div>
-      <Typography>Home</Typography>
-    </div>
+    <Container>
+      {/* <Typography>Home</Typography> */}
+      <Box mt={5}>
+        <img
+          src="https://i.pinimg.com/originals/d1/40/fa/d140fa7454523f708c72d2242a9c970d.png"
+          alt="image"
+          width={1100}
+        />
+      </Box>
+    </Container>
   );
 }
 
