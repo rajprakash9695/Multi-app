1dd0c176719fb4e073cbf100465f8dda1e5c40b7
Raj Prakash Maurya <raj.maurya@bksgold.com>
2023-10-30 15:57:01 +0530

    local stroage app completed

--- src/Components/LocalStorage.jsx
+++ src/Components/LocalStorage.jsx
@@ -1,12 +1,48 @@
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
-import { useLocalStorage } from "../hooks/useLocalStorage";
+import React, { useEffect, useState } from "react";
+// import { useLocalStorage } from "../hooks/useLocalStorage";
+
+const getLocalItems = () => {
+  let item = localStorage.getItem("lists");
+
+  if (item) {
+    return JSON.parse(localStorage.getItem("lists"));
+  } else {
+    return [];
+  }
+};
 
 function LocalStorage() {
   const [value, setValue] = useState("");
-  console.log(value);
+  const [data, setData] = useState([]);
+  const [getdata, setGetData] = useState();
+  // console.log(value);
 
-  const localData = useLocalStorage();
+  const handleSetLocal = () => {
+    let result = setData(value);
+    console.log(result);
+    setValue("");
+    alert("Your input is set to local storage");
+  };
+
+  const handleGetLocal = () => {
+    // console.log("get");
+    let result = getLocalItems();
+    setGetData(result);
+    // console.log("ok");
+  };
+
+  const handleRemoveLocal = () => {
+    localStorage.removeItem("lists");
+    alert("Item is removed from local storage");
+    // console.log("removed");
+  };
+
+  // const localData = useLocalStorage();
+
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(data));
+  }, [data]);
 
   return (
     <Container maxWidth="md">
@@ -23,11 +59,17 @@ function LocalStorage() {
           onChange={(e) => setValue(e.target.value)}
         />
         <Box display={"flex"} gap={1} justifyContent={"center"}>
-          <Button variant="contained">Set Local</Button>
-          <Button variant="contained">Get Local</Button>
-          <Button variant="contained">Remove From Local</Button>
+          <Button onClick={handleSetLocal} variant="contained">
+            Set Local
+          </Button>
+          <Button onClick={handleGetLocal} variant="contained">
+            Get Local
+          </Button>
+          <Button variant="contained" onClick={handleRemoveLocal}>
+            Remove From Local
+          </Button>
         </Box>
-        <Box> jkasdhkds</Box>
+        <Box>{getdata} </Box>
       </Box>
     </Container>
   );
