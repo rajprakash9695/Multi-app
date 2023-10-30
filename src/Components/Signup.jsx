0ed652b8395979c900bb053b45bfbb072b359483
Raj Prakash Maurya <raj.maurya@bksgold.com>
2023-10-30 18:42:34 +0530

    firebase is added to Signup Form

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,95 +1,3 @@
-// import {
-//   Box,
-//   Button,
-//   Card,
-//   CardContent,
-//   CardHeader,
-//   Container,
-//   TextField,
-//   Typography,
-// } from "@mui/material";
-// import React, { useState } from "react";
-
-// function Signup() {
-//   const [initialValue, setInitialValue] = useState({
-//     name: "",
-//     password: "",
-//     email: "",
-//   });
-
-//   const [value, setValue] = useState(null);
-
-//   //   console.log("log", value);
-//   //   console.log("log", initialValue);
-//   const handleChange = (e) => {
-//     // setInitialValue(e.target.value);
-//     // console.log("ok");
-
-//     const { name, value } = e.target;
-//     setInitialValue((prevData) => ({
-//       ...prevData,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // setInitialValue({ ...initialValue });
-//     // console.log(initialValue);
-//     setValue({ ...initialValue });
-//   };
-
-//   return (
-//     <Box textAlign={"center"}>
-//       <Container maxWidth={"sm"}>
-//         <Card>
-//           <CardHeader title="Signup" />
-//           <CardContent
-//             sx={{
-//               display: "flex",
-//               flexDirection: "column",
-//               gap: "15px",
-//             }}
-//           >
-//             <form onSubmit={handleSubmit}>
-//               <TextField
-//                 variant="outlined"
-//                 fullWidth
-//                 label="Name"
-//                 name="name"
-//                 value={initialValue.name}
-//                 onChange={handleChange}
-//               />
-//               <TextField
-//                 variant="outlined"
-//                 type="password"
-//                 fullWidth
-//                 label="Password"
-//                 name="password"
-//                 value={initialValue.password}
-//                 onChange={handleChange}
-//               />
-//               <TextField
-//                 variant="outlined"
-//                 fullWidth
-//                 type="email"
-//                 name="email"
-//                 value={initialValue.email}
-//                 onChange={handleChange}
-//                 label="Email"
-//               />
-//               <Button onSubmit={handleSubmit}>Submit</Button>
-//             </form>
-//             {value && <Typography> Email: {value.name}</Typography>}
-//           </CardContent>
-//         </Card>
-//       </Container>
-//     </Box>
-//   );
-// }
-
-// export default Signup;
-
 import React, { useEffect, useState } from "react";
 import {
   Box,
@@ -112,18 +20,83 @@ function Signup() {
 
   const [displayData, setDisplayData] = useState(null);
 
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setInputData((prevData) => ({
+  //     ...prevData,
+  //     [name]: value,
+  //   }));
+  // };
+
+  let name, value;
+
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setInputData((prevData) => ({
-      ...prevData,
+    name = e.target.name;
+    value = e.target.value;
+    setInputData({
+      ...inputData,
       [name]: value,
-    }));
+    });
   };
   // console.log("log", inputData);
   // console.log("log1", displayData);
-  const handleSubmit = (e) => {
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setDisplayData({ ...inputData });
+  // };
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setDisplayData({ ...inputData });
+
+    console.log("Submit Click");
+
+    const { name, password, email, cfPassword } = inputData;
+
+    if (name && email && password && cfPassword) {
+      const res = await fetch(
+        "https://signup-form-c349c-default-rtdb.firebaseio.com/signup-form.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            password,
+            email,
+            cfPassword,
+          }),
+        }
+      );
+      if (res) {
+        setInputData({
+          name: "",
+          password: "",
+          email: "",
+          cfPassword: "",
+        });
+        alert("data stored SuccesFully");
+      }
+    } else {
+      alert("please fill all the data");
+    }
+  };
+
+  const handleGetData = async (e) => {
+    e.preventDefault();
+    console.log("Get data");
+    const res = await fetch(
+      "https://signup-form-c349c-default-rtdb.firebaseio.com/signup-form.json",
+      {
+        method: "Get",
+      }
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        console.log("ok");
+        setDisplayData(data);
+        console.log(displayData);
+        console.log("ok2");
+      });
   };
 
   return (
@@ -138,7 +111,7 @@ function Signup() {
               gap: "15px",
             }}
           >
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} method="POST">
               <Box display={"flex"} flexDirection={"column"} gap={2}>
                 <TextField
                   variant="outlined"
@@ -175,18 +148,32 @@ function Signup() {
                   value={inputData.cfPassword}
                   onChange={handleChange}
                 />
-                <Button type="submit">Submit</Button>
+                <Button type="submit" onClick={handleSubmit}>
+                  Submit
+                </Button>
               </Box>
             </form>
-            {displayData && (
+            {/* {displayData && (
               <Typography>
                 Name: {displayData.name}, Email: {displayData.email} , Password:{" "}
                 {displayData.password} , Confirm Password:{" "}
                 {displayData.cfPassword}
               </Typography>
-            )}
-          </CardContent>
+            )} */}
+          </CardContent>{" "}
         </Card>
+        <Button type="submit" onClick={handleGetData}>
+          Get data
+        </Button>
+        <Box>
+          {displayData && (
+            <Typography>
+              Name: {displayData.name}, Email: {displayData.email} , Password:{" "}
+              {displayData.password} , Confirm Password:{" "}
+              {displayData.cfPassword}
+            </Typography>
+          )}
+        </Box>
       </Container>
     </Box>
   );
